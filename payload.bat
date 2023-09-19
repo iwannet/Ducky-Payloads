e97e888169e387ba4822411554ce073248073503
iwannet69 <95164143+iwannet@users.noreply.github.com>
2023-09-19 17:37:32 +0000

    Update payload.bat

--- payload.bat
+++ payload.bat
@@ -1,1 +1,13 @@
-msg * Test
+@echo off
+start /max "" https://fakeupdate.net/win10ue/
+
+
+echo Set WshShell = WScript.CreateObject("WScript.Shell") > %temp%\temp.vbs
+echo WshShell.SendKeys "{F1}{F11}" >> %temp%\temp.vbs
+
+timeout /t 2 >nul
+start %temp%\temp.vbs
+
+timeout /t 1 >nul
+del %temp%\temp.vbs
+
