c72fd6f492ea5041042e0a897c457f1fe8d528c0
iwannet69 <95164143+iwannet@users.noreply.github.com>
2023-09-19 15:45:35 +0000

    Just test payload.dd

--- payload.ps1
+++ payload.ps1
@@ -1,1 +1,11 @@
+Add-Type -TypeDefinition @"
+using System;
+using System.Windows.Forms;
+public class PopupMessage {
+    public static void Main() {
+        MessageBox.Show("Hello", "Popup Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+}
+"@
 
+[PopupMessage]::Main()
